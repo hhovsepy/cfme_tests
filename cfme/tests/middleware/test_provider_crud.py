671f6c542403c90249938f436a160f19ff365bee
Hayk Hovsepyan <hhovsepy@redhat.com>
2016-04-08 15:15:42 +0200

    Added test middleware provider CRUD

--- cfme/tests/middleware/test_provider_crud.py
+++ cfme/tests/middleware/test_provider_crud.py
@@ -1,20 +1,45 @@
+import uuid
 import pytest
-from cfme import login
-from utils import providers
 
-login.login_admin()
+from utils import testgen
+from utils.providers import setup_a_provider as _setup_a_provider
+from utils.update import update
+from utils.version import current_version
+
+pytestmark = pytest.mark.uncollectif(lambda: current_version() < "5.5")
+
+pytest_generate_tests = testgen.generate(testgen.middleware_providers, scope="function")
 
 
-@pytest.fixture(scope='function')
-def hawkular_provider():
-    return providers.get_crud('hawkular')
+@pytest.fixture(scope="function")
+def a_middleware_provider():
+    return _setup_a_provider("mjddleware")
 
 
 @pytest.mark.usefixtures('has_no_middleware_providers')
-def test_hawkular_crud(hawkular_provider):
-    """Test provider add with good credentials.
+def test_provider_crud(request, provider):
+    """ Tests provider add with good credentials
 
-    has_no_middleware_providers fixture is not yet implemented.  You must manually remove
-    added providers between test runs
+    Metadata:
+        test_flag: crud
     """
-    hawkular_provider.create(cancel=False, validate_credentials=False)
+    provider.create()
+    provider.validate_stats(ui=True)
+
+    old_name = provider.name
+    with update(provider):
+        provider.name = str(uuid.uuid4())  # random uuid
+
+    with update(provider):
+        provider.name = old_name  # old name
+
+    provider.delete(cancel=False)
+    provider.wait_for_delete()
+
+
+def test_provider_edit_port(request, a_middleware_provider):
+    old_port = a_middleware_provider.port
+    request.addfinalizer(lambda: a_middleware_provider.update({'port': old_port}))
+    with update(a_middleware_provider):
+        a_middleware_provider.port = '1234'
+    assert str(a_middleware_provider.port) == a_middleware_provider.get_detail('Properties', 'Port')
